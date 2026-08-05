5b7f29a00c6873c02cc44035edeb9daf877d0e63
vm <vm@gmail.com>
2026-08-05 06:27:55 +0530

    Set founder storefront as homepage

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["razorpay"],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/c/founder",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
